d6746036b0db735a94ea6700b6caeda4b0b545e4
kush1509 <kushagra.goel.15@gmail.com>
2017-10-15 01:30:54 +0800

    date error

--- routes/transactions.js
+++ routes/transactions.js
@@ -13,7 +13,7 @@ router.post('/addTransaction', function(req, res) {
 	var modeOfPayment = req.body.modeOfPayment;
 	var uniqueId = req.body.uniqueId;
 
-	console.log(date);
+	//console.log(date);
 
 	Transaction.create({
 		typeOfTransaction: typeOfTransaction,
